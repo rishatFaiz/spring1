8102dab92a7e9d0abab125f68c5362fc128ec34c
r.faizrakhmanov <r.faizrakhmanov@iis-soft.ru>
2020-01-20 16:23:06 +0500

    #add part of authentication

--- authorizationServer/src/main/java/ru/lean/authorization/AuthorizationServer.java
+++ authorizationServer/src/main/java/ru/lean/authorization/AuthorizationServer.java
@@ -2,11 +2,13 @@ package ru.lean.authorization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class AuthorizationServer {
 
-  public static void maint(String[] args) {
+  public static void main(String[] args) {
     SpringApplication.run(AuthorizationServer.class, args);
   }
 }
--- authorizationServer/src/main/java/ru/lean/authorization/config/AuthorizationServerConfig.java
+++ authorizationServer/src/main/java/ru/lean/authorization/config/AuthorizationServerConfig.java
@@ -2,53 +2,85 @@ package ru.lean.authorization.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.DatabasePopulator;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.approval.UserApprovalHandler;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
-  private static final int THIRTY_DAYS = 60 * 60 * 24 * 30;
-  private static String REALM="CRM_REALM";
-  @Autowired
-  private TokenStore tokenStore;
 
-  @Autowired
-  private UserApprovalHandler userApprovalHandler;
+  @Value("classpath:ru/lean/authorization/config/schema.sql")
+  private Resource schemaScript;
 
   @Autowired
   @Qualifier("authenticationManagerBean")
   private AuthenticationManager authenticationManager;
 
-  @Override
-  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-    clients.inMemory().withClient("testClient")
-        .secret("secret")
-        .authorizedGrantTypes("password", "refresh_token","client_credentials")
-        .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-        .scopes("read", "write", "trust")
-        .accessTokenValiditySeconds(300)
-        .refreshTokenValiditySeconds(THIRTY_DAYS);
-  }
+  private DataSource dataSource;
 
-  @Override
-  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-    endpoints.tokenStore(tokenStore).userApprovalHandler(userApprovalHandler)
-        .authenticationManager(authenticationManager);
-
-
+  public AuthorizationServerConfig(DataSource dataSource) {
+    this.dataSource = dataSource;
   }
 
   @Override
   public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
-    oauthServer.realm(REALM);
-    oauthServer.checkTokenAccess("permitAll()");
+    oauthServer.tokenKeyAccess("permitAll()")
+        .checkTokenAccess("isAuthenticated()");
   }
+
+  @Override
+  public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+    clients.jdbc(dataSource)
+        .withClient("client-service")
+        .authorizedGrantTypes("implicit")
+        .scopes("read")
+        .autoApprove(true)
+        .and()
+        .withClient("article-service")
+        .secret("secret")
+        .authorizedGrantTypes(
+            "password","authorization_code", "refresh_token")
+        .scopes("read");
+  }
+
+  @Override
+  public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+    endpoints.tokenStore(tokenStore())
+        .authenticationManager(authenticationManager);
+  }
+
+  @Bean
+  public TokenStore tokenStore() {
+    return new JdbcTokenStore(dataSource);
+  }
+
+  @Bean
+  public DataSourceInitializer dataSourceInitializer() {
+    DataSourceInitializer initializer = new DataSourceInitializer();
+    initializer.setDataSource(dataSource);
+    initializer.setDatabasePopulator(databasePopulator());
+    return initializer;
+  }
+
+  private DatabasePopulator databasePopulator() {
+    ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+    populator.addScript(schemaScript);
+    return populator;
+  }
+
 }
